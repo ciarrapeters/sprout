cac787bd940b7e2f45cbe81ca506ec51f3907bf5
lukejanik <lukecjanik@gmail.com>
2020-02-08 12:45:02 -0500

    boxlist form

--- src/components/BoxListComponent.js
+++ src/components/BoxListComponent.js
@@ -30,24 +30,30 @@ class BoxListComponent extends React.Component {
 
   render() {
       return(
-			
+				
           <div>
 						
 						<BoxList activities={this.state.activities} removeActivity={this.removeActivity}/>
 						<form onSubmit = {(e) => this.addActivity(e)}>
-			
+					
 							<div class="input-group">
+							<div className="col-sm-6">
 								<input type="text" class="form-control" id="inputActivity" placeholder="Activity" value={this.state.text}
 											onChange={(e) => {this.updateValue(e)}}/>
+								</div>
+			
+				
 								<span class="input-group-btn">
 									<button type="button" class="btn btn-primary">Add activity</button>
 								</span>
-							</div>	
+
+							</div>
+							
 
 						</form> 
-					
-          </div>
 				
+          </div>
+			
       );
   }
 }
@@ -61,9 +67,11 @@ class BoxList extends React.Component {
 	render() {
 			return(
 					<ul>
+						
 							{ this.props.activities.map((activity,i) => {
 									return <p onClick={() => { this.removeItem(activity, i)}} key={i}>{ activity }</p>
 							})}
+						
 					</ul>
 			);
 	}
