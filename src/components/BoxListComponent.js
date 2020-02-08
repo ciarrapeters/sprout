1027d9fe4b9eaf4d8685306b681283929db324a3
lukejanik <lukecjanik@gmail.com>
2020-02-08 12:32:16 -0500

    box list component start

--- src/components/BoxListComponent.js
+++ src/components/BoxListComponent.js
@@ -1,11 +1,73 @@
 import React from 'react';
 
 class BoxListComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { activities: [], text: '' };
+    this.removeActivity = this.removeActivity.bind(this);
+  }
+
+  addActivity(e) {
+      e.preventDefault();
+      this.setState({ 
+        activities: [ this.state.text, ...this.state.activities ],
+        text: ''
+      });
+  }
+
+  removeActivity(name, i){
+      let activities = this.state.activities.slice();
+      activities.splice(i, 1);
+      this.setState({
+          activities
+      });
+  }
+
+  updateValue(e) {
+      this.setState({ text: e.target.value})
+  }
+
   render() {
-		return (
-			<h1>hi box list component</h1>
-		)
+      return(
+			
+          <div>
+						
+						<BoxList activities={this.state.activities} removeActivity={this.removeActivity}/>
+						<form onSubmit = {(e) => this.addActivity(e)}>
+			
+							<div class="input-group">
+								<input type="text" class="form-control" id="inputActivity" placeholder="Activity" value={this.state.text}
+											onChange={(e) => {this.updateValue(e)}}/>
+								<span class="input-group-btn">
+									<button type="button" class="btn btn-primary">Add activity</button>
+								</span>
+							</div>	
+
+						</form> 
+					
+          </div>
+				
+      );
+  }
+}
+
+class BoxList extends React.Component {
+
+	removeItem(item, i) {
+			this.props.removeActivity(item, i);
+	}
+
+	render() {
+			return(
+					<ul>
+							{ this.props.activities.map((activity,i) => {
+									return <p onClick={() => { this.removeItem(activity, i)}} key={i}>{ activity }</p>
+							})}
+					</ul>
+			);
 	}
 }
 
 export default BoxListComponent;
+
