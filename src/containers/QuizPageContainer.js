16b05cac66003a6058710cd53a788f560ebc61c9
lukejanik <lukecjanik@gmail.com>
2020-02-08 21:13:22 -0500

    fixing quiz

--- src/containers/QuizPageContainer.js
+++ src/containers/QuizPageContainer.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import ImagePicker from 'react-image-picker'
+import 'react-image-picker/dist/index.css'
+
+
+import taco from '../images/taco.png'
+import sushi from '../images/sushi.png'
+import pizza from '../images/pizza.png'
+import chinese from '../images/chinese.png'
+
+import movies from '../images/movies.png'
+import museum from '../images/museum.png'
+import sports from '../images/sports.png'
+import nature from '../images/tree.png'
+
+const foodList = [taco, sushi, pizza, chinese]
+const activityList = [movies, museum, sports, nature]
 
 class QuizPageContainer extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {values: []};
-
+		this.onPick = this.onPick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	
 	handleChange(event) {
+		console.log(event.target.value)
     this.setState({value: event.target.value});
 	}
 	
 	handleSubmit(event) {
     // alert('Your food choice is: ' + this.state.value);
-		// event.preventDefault();
-		this.props.toggle('home')
+		event.preventDefault();
+		this.props.toggle('home');
+	}
+	onPick(image) {
+    this.setState({image});
 	}
 	
 	render() {
@@ -30,31 +50,27 @@ class QuizPageContainer extends React.Component {
 	                Ciarra and Luke
 	            </span>
 	        </nav>
-			<h2>Take this quiz to find common interests with your friend!</h2>
+	
+			<h2>Fill out this quiz to match activities with a friend!</h2>
+
 			<form onSubmit={this.handleSubmit}>
-			<label>Pick your favorite food:</label>
+			<h4>What's your favorite food?</h4>
 			<div>
+        <ImagePicker 
+          images={foodList.map((image, i) => ({src: image, value: i}))}
+					onPick={this.onPick}
+					onChange={this.handleChange}
+        />
+      </div>
 
-			<label><input type="checkbox" name="check_list[]" value="italian" onChange={this.handleChange}/><img src={require("../images/pizza.png")} alt="pizza" class="img-thumbnail"></img></label>
-
-			<label><input type="checkbox" name="check_list[]" value="mexican" onChange={this.handleChange}/><img src={require("../images/taco.jpeg")} alt="taco" class="img-thumbnail"></img></label>
-
-			<label><input type="checkbox" name="check_list[]" value="sushi" onChange={this.handleChange}/><img src={require("../images/sushi.png")} alt="sushi" class="img-thumbnail"></img></label>
-
-			<label><input type="checkbox" name="check_list[]" value="chinese" onChange={this.handleChange}/><img src={require("../images/chinese.png")} alt="chinese" class="img-thumbnail"></img></label>
-		
-			</div>
-			<label>Pick your favorite activity:</label>
+			<h4>What's your favorite activity?</h4>
 			<div>
-			<label><input type="checkbox" name="check_list[]" value="sports" onChange={this.handleChange}/><img src={require("../images/sports.png")} alt="sports" class="img-thumbnail"></img></label>
-
-			<label><input type="checkbox" name="check_list[]" value="museum" onChange={this.handleChange}/><img src={require("../images/museum.png")} alt="museum" class="img-thumbnail"></img></label>
-
-			<label><input type="checkbox" name="check_list[]" value="movies" onChange={this.handleChange}/><img src={require("../images/movies.png")} alt="movies" class="img-thumbnail"></img></label>
-
-			</div>
-			
-			<button type="submit" class="btn btn-primary">Submit!</button>
+        <ImagePicker 
+          images={activityList.map((image, i) => ({src: image, value: i}))}
+          onPick={this.onPick}
+        />
+      </div>
+			<button type="submit" class="btn btn-outline-info">Submit!</button>
 			</form>
 			</React.Fragment>
 			
